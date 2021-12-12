daa345facdc3f7cf51e1027ec975a4d632f786a2
janshercs <jansen.cs@gmail.com>
2021-12-12 12:07:13 +0800

    Allow backend url for dev and prod environments.

--- frontend/src/chat_components/Chatbox.js
+++ frontend/src/chat_components/Chatbox.js
@@ -3,29 +3,26 @@ import InputArea from "./InputArea";
 import { useEffect, useState } from "react";
 import { VStack } from "@chakra-ui/layout";
 
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://wassup-bub.herokuapp.com";
 
 export default function Chatbox() {
   const [threads, setThreads] = useState([]);
 
   useEffect(() => {
     initWS();
-    // async function fetchThreads() {
-    //   let response = await fetch(BACKEND_URL + "/thread");
-    //   response = await response.json();
-    //   setThreads(response);
-    // }
-    // fetchThreads();
   }, []);
 
   function initWS() {
-    let socket = new WebSocket("ws://localhost:5000/ws");
+    let wsURL = BACKEND_URL.replace(/^https?/, "ws") + "/ws";
+    let socket = new WebSocket(wsURL);
     socket.onopen = function () {
       console.log("Socket is open");
     };
     socket.onmessage = function (e) {
       setThreads(JSON.parse(e.data));
-      // console.log(JSON.parse(e.data));
     };
     return socket;
   }
